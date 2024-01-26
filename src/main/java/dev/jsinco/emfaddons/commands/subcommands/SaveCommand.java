7dd18c1a20195f4a2a98c5059b42973819b6bd68
jonah <jonahmarksinclair@gmail.com>
2024-01-26 10:37:34 -0500

    Commit

--- src/main/java/dev/jsinco/emfaddons/commands/subcommands/SaveCommand.java
+++ src/main/java/dev/jsinco/emfaddons/commands/subcommands/SaveCommand.java
@@ -1,6 +1,7 @@
 package dev.jsinco.emfaddons.commands.subcommands;
 
 import dev.jsinco.emfaddons.EMFAddons;
+import dev.jsinco.emfaddons.Util;
 import dev.jsinco.emfaddons.commands.SubCommand;
 import dev.jsinco.emfaddons.files.EMFFile;
 import org.bukkit.command.CommandSender;
@@ -11,7 +12,7 @@ public class SaveCommand implements SubCommand {
     @Override
     public void execute(EMFAddons plugin, CommandSender sender, String[] args) {
         EMFAddons.getSQLite().saveCachedPlayers();
-        sender.sendMessage(new EMFFile("messages.yml").getFile().getString("prefix") + "Saved all players to database.");
+        sender.sendMessage(Util.colorcode(new EMFFile("messages.yml").getFile().getString("prefix") + "Saved all players to database."));
     }
 
     @Override
